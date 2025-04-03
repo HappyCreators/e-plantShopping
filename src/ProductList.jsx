2cad2c479eafd1a19cfd80152faf5a7f782aba05
HappyCreators <bestself.happysoul@gmail.com>
2025-04-02 22:29:18 -0400

    ProductList.jsx needs to be fixed, cart functions are not working

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
+import { useDispatch } from "react-redux";
 import CartItem from './CartItem';
 import { addItem } from "./CartSlice";
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const (addedToCart, setAddedToCart) = useState({});
+    const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
